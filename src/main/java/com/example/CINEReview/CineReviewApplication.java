7791c419003149d640d47f5b31772619e3de667d
LeeJin0527 <ijin17463@gmail.com>
2022-02-17 14:14:21 +0900

    초기 셋팅:build.gradle, application.properties

---
+++ src/main/java/com/example/CINEReview/CineReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.CINEReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CINEReview/CineReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CINEReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
